d79a7cbb6678067b15f2edf07ed48bb3b3320776
Luiz Nonato <gabriel.naato@gmail.com>
2023-09-13 16:52:54 -0300

    primeira versão do bot

---
+++ document.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histórico de pedido</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Valor do Banco de Dados</h1>
+        <p>O valor recuperado do banco de dados é: <?php echo $msg; ?></p>
+    </div>
+</body>
+</html>
